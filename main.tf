2a5a7965b5428a2ccddaba4f74bd296b14fdf4b8
JayTee305 <yehoshua305@gmail.com>
2021-01-16 21:55:08 +0100

    first commit

---
+++ main.tf
@@ -0,0 +1,45 @@
+
+provider "aws" {
+  region = "us-west-2"
+}
+
+provider "random" {}
+
+resource "random_pet" "name" {}
+
+resource "aws_instance" "web" {
+  ami                    = "ami-a0cfeed8"
+  instance_type          = "t2.micro"
+  user_data              = file("init-script.sh")
+  vpc_security_group_ids = [aws_security_group.web-sg.id]
+
+  tags = {
+    Name = random_pet.name.id
+  }
+}
+
+output "domain-name" {
+  value = aws_instance.web.public_dns
+}
+
+output "application-url" {
+  value = "${aws_instance.web.public_dns}/index.php"
+}
+
+resource "aws_security_group" "web-sg" {
+  name = "${random_pet.name.id}-sg"
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
