223c7fbdd1bc3ef89b95a8923680cb5e01343c0e
Joshua Teye <joshuateye@Joshs-Mac.local>
2021-02-10 20:01:16 +0100

    add new ec2 instance

--- main.tf
+++ main.tf
@@ -18,6 +18,17 @@ resource "aws_instance" "web" {
   }
 }
 
+resource "aws_instance" "web2" {
+  ami                    = "ami-a0cfeed8"
+  instance_type          = "t2.micro"
+  user_data              = file("init-script.sh")
+  vpc_security_group_ids = [aws_security_group.web-sg.id]
+
+  tags = {
+    Name = random_pet.name.id
+  }
+}
+
 output "domain-name" {
   value = aws_instance.web.public_dns
 }
